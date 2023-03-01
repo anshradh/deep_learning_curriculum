4f18ad50aad2f0bba4c13ef81ce29be5d7cf3899
Ansh Radhakrishnan <anshuman.radhakrishnan@yale.edu>
2023-03-01 14:39:51 -0800

    Add generation

--- exercises/transformers/model.py
+++ exercises/transformers/model.py
@@ -31,9 +31,13 @@ class GPTConfig:
 
     def __post_init__(self):
         if self.d_head is not None:
-            assert self.d_model // self.n_heads == self.d_head, "d_model must be divisible by n_heads into d_head"
+            assert (
+                self.d_model // self.n_heads == self.d_head
+            ), "d_model must be divisible by n_heads into d_head"
         else:
-            assert self.d_model % self.n_heads == 0, "d_model must be divisible by n_heads"
+            assert (
+                self.d_model % self.n_heads == 0
+            ), "d_model must be divisible by n_heads"
             self.d_head = self.d_model // self.n_heads
         if self.d_mlp is None:
             self.d_mlp = 4 * self.d_model
@@ -64,7 +68,9 @@ class Unembed(nn.Module):
     def __init__(self, config: GPTConfig):
         super().__init__()
         self.config = config
-        assert config.d_vocab_out is not None, "d_vocab_out must be specified for unembedding"
+        assert (
+            config.d_vocab_out is not None
+        ), "d_vocab_out must be specified for unembedding"
         self.W_U = nn.Parameter(torch.empty(config.d_vocab_out, config.d_model))
         self.reset_parameters()
 
@@ -90,7 +96,10 @@ class PosEmbed(nn.Module):
         self.W_P = self.W_P.to(self.config.device)
 
     def forward(self, x: torch.Tensor, cache_offset: int = 0) -> torch.Tensor:
-        return F.embedding(torch.arange(cache_offset, x.shape[1] + cache_offset, device=x.device), self.W_P)
+        return F.embedding(
+            torch.arange(cache_offset, x.shape[1] + cache_offset, device=x.device),
+            self.W_P,
+        )
 
 
 class RMSNorm(nn.Module):
@@ -101,7 +110,9 @@ class RMSNorm(nn.Module):
         self.config = config
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return x / torch.sqrt(torch.mean(x ** 2, dim=-1, keepdim=True) + self.config.norm_eps)
+        return x / torch.sqrt(
+            torch.mean(x ** 2, dim=-1, keepdim=True) + self.config.norm_eps
+        )
 
 
 @dataclass
@@ -115,8 +126,12 @@ class KVCacheEntry:
     def init_entry(cls, config: GPTConfig, batch_size: int):
         assert config.d_head is not None
         return cls(
-            past_k=torch.empty(batch_size, config.n_heads, 0, config.d_head, device=config.device),
-            past_v=torch.empty(batch_size, config.n_heads, 0, config.d_head, device=config.device),
+            past_k=torch.empty(
+                batch_size, config.n_heads, 0, config.d_head, device=config.device
+            ),
+            past_v=torch.empty(
+                batch_size, config.n_heads, 0, config.d_head, device=config.device
+            ),
         )
 
     def update(self, k: torch.Tensor, v: torch.Tensor):
@@ -139,7 +154,12 @@ class KVCache:
 
     @classmethod
     def init_cache(cls, config: GPTConfig, batch_size: int):
-        return cls([KVCacheEntry.init_entry(config, batch_size) for _ in range(config.n_layers)])
+        return cls(
+            [
+                KVCacheEntry.init_entry(config, batch_size)
+                for _ in range(config.n_layers)
+            ]
+        )
 
 
 class Attention(nn.Module):
@@ -149,10 +169,18 @@ class Attention(nn.Module):
         super().__init__()
         self.config = config
         assert config.d_head is not None
-        self.W_Q = nn.Parameter(torch.empty((config.n_heads, config.d_head, config.d_model)))
-        self.W_K = nn.Parameter(torch.empty((config.n_heads, config.d_head, config.d_model)))
-        self.W_V = nn.Parameter(torch.empty((config.n_heads, config.d_head, config.d_model)))
-        self.W_O = nn.Parameter(torch.empty((config.n_heads, config.d_model, config.d_head)))
+        self.W_Q = nn.Parameter(
+            torch.empty(config.n_heads, config.d_head, config.d_model)
+        )
+        self.W_K = nn.Parameter(
+            torch.empty(config.n_heads, config.d_head, config.d_model)
+        )
+        self.W_V = nn.Parameter(
+            torch.empty(config.n_heads, config.d_head, config.d_model)
+        )
+        self.W_O = nn.Parameter(
+            torch.empty(config.n_heads, config.d_model, config.d_head)
+        )
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -163,29 +191,63 @@ class Attention(nn.Module):
     def forward(self, x: torch.Tensor, cache_entry: Optional[KVCacheEntry] = None):
         seq_len = x.shape[1]
 
-        q = einsum("... seq d_model, n_heads d_head d_model -> ... n_heads seq d_head", x, self.W_Q)
-        k = einsum("... seq d_model, n_heads d_head d_model -> ... n_heads seq d_head", x, self.W_K)
-        v = einsum("... seq d_model, n_heads d_head d_model -> ... n_heads seq d_head", x, self.W_V)
+        q = einsum(
+            "... seq d_model, n_heads d_head d_model -> ... n_heads seq d_head",
+            x,
+            self.W_Q,
+        )
+        k = einsum(
+            "... seq d_model, n_heads d_head d_model -> ... n_heads seq d_head",
+            x,
+            self.W_K,
+        )
+        v = einsum(
+            "... seq d_model, n_heads d_head d_model -> ... n_heads seq d_head",
+            x,
+            self.W_V,
+        )
 
         if cache_entry is not None:
-            assert seq_len == 1, "Cache can only be used for single step decoding"
+            cache_start = cache_entry.past_k.shape[2]
+            if cache_start > 0:
+                assert seq_len == 1, "Cache can only be used for single step decoding"
+
             k, v = cache_entry.update(k, v)
-            cache_start = k.shape[2] - 1
+            cache_start = k.shape[2]
         else:
             cache_start = 0
 
-        attn = einsum("... seq_q d_head, ... seq_k d_head -> ... seq_q seq_k", q, k,)
-        attn = attn / (self.config.d_head ** 0.5) # type: ignore
-        mask = torch.arange(seq_len, device=attn.device)[None, :] <= torch.arange(cache_start, cache_start + seq_len, device=attn.device)[:, None]
-        attn = attn.masked_fill(~mask, -1e9)
+        attn = einsum(
+            "... seq_q d_head, ... seq_k d_head -> ... seq_q seq_k",
+            q,
+            k,
+        )
+        attn = attn / (self.config.d_head ** 0.5)  # type: ignore
+        mask = torch.arange(seq_len, device=attn.device,)[None, :] <= torch.arange(
+            cache_start, cache_start + seq_len, device=attn.device
+        )[:, None]
+        attn.masked_fill_(~mask, -1e9)
         attn = F.softmax(attn, dim=-1)
 
-        combined_v = attn @ v
-        out = einsum("... n_heads seq d_head, n_heads d_model d_head -> ... n_heads seq d_model", combined_v, self.W_O)
-        out = einops.reduce(out, "... n_heads seq d_model -> ... seq d_model", "sum")
+        combined_v = einsum(
+            "... seq_q seq_k, ... seq_k d_head -> ... seq_q d_head",
+            attn,
+            v,
+        )
+
+        out = einsum(
+            "... n_heads seq d_head, n_heads d_model d_head -> ... n_heads seq d_model",
+            combined_v,
+            self.W_O,
+        )
+
+        out = einops.reduce(
+            out,
+            "... n_heads seq d_model -> ... seq d_model",
+            "sum",
+        )
 
         return out
-
 
 
 class MLP(nn.Module):
@@ -222,7 +284,11 @@ class Block(nn.Module):
         self.mlp = MLP(config)
         self.norm_2 = RMSNorm(config)
 
-    def forward(self, x: torch.Tensor, cache_entry: Optional[KVCacheEntry] = None):
+    def forward(
+        self,
+        x: torch.Tensor,
+        cache_entry: Optional[KVCacheEntry] = None,
+    ):
         x = x + self.attn(self.norm_1(x), cache_entry)
         x = x + self.mlp(self.norm_2(x))
         return x
@@ -240,12 +306,81 @@ class GPT(nn.Module):
         self.final_norm = RMSNorm(config)
         self.unembed = Unembed(config)
 
-
     def forward(self, x: torch.Tensor, cache: Optional[KVCache] = None):
-        seq_len = x.shape[1]
-        x = self.tok_emb(x) + self.pos_emb(x, cache[0].past_k.shape[2] if cache is not None else 0)
+        x = self.tok_emb(x) + self.pos_emb(
+            x,
+            cache[0].past_k.shape[2] if cache is not None else 0,
+        )
         for i, block in enumerate(self.blocks):
-            x = block(x, cache[i] if cache is not None else None)
+            x = block(
+                x,
+                cache[i] if cache is not None else None,
+            )
         x = self.final_norm(x)
         x = self.unembed(x)
         return x
+
+    @torch.no_grad()
+    def generate(
+        self,
+        prompt: torch.Tensor,
+        max_new_tokens: int = 100,
+        sample: bool = False,
+        temperature: float = 1.0,
+        top_k: Optional[int] = None,
+        top_p: Optional[float] = None,
+        use_cache: bool = True,
+    ):
+        batch_size, seq_len = prompt.shape
+
+        if use_cache:
+            cache = KVCache.init_cache(self.config, prompt.shape[0])
+        else:
+            cache = None
+
+        out = prompt
+        for i in range(max_new_tokens):
+            if prompt.shape[-1] > self.config.n_ctx:
+                break
+            logits = self(prompt, cache)
+            logits = logits[:, -1, :]
+            if sample:
+                if top_k is not None:
+                    top_k_logits, top_k_indices = torch.topk(
+                        logits,
+                        top_k,
+                        dim=-1,
+                    )
+                    logits = torch.where(
+                        logits < top_k_logits[:, -1:],
+                        torch.full_like(logits, -1e9),
+                        logits,
+                    )
+
+                if top_p is not None:
+                    probs = F.softmax(logits, dim=-1)
+                    sorted_probs, sorted_indices = torch.sort(
+                        probs,
+                        descending=True,
+                    )
+                    cumulative_probs = torch.cumsum(sorted_probs, dim=-1)
+                    sorted_indices_to_remove = cumulative_probs > top_p
+                    logits = torch.where(
+                        sorted_indices_to_remove,
+                        torch.full_like(logits, -1e9),
+                        logits,
+                    )
+
+                probs = F.softmax(logits / temperature, dim=-1)
+                next_token = torch.multinomial(probs, num_samples=1)
+            else:
+                next_token = logits.argmax(dim=-1, keepdim=True)
+
+            prompt = (
+                next_token
+                if cache is not None
+                else torch.cat([prompt, next_token], dim=1)
+            )
+            out = torch.cat([out, next_token], dim=1)
+
+        return out
