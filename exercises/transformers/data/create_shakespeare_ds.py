decc3e8b3fa4c54f1a9cc0bc6f2a83ea749de2a5
Ansh Radhakrishnan <anshuman.radhakrishnan@yale.edu>
2023-03-03 10:10:29 -0800

    Clean up dataset and repo

--- exercises/transformers/data/create_shakespeare_ds.py
+++ exercises/transformers/data/create_shakespeare_ds.py
@@ -1,22 +1,33 @@
+"""Creates a dataset of shakespeare's works."""
 import torch
 import re
+import requests
 
-f = open("shakespeare.txt", "r")
-content = f.read()
+response = requests.get("https://www.gutenberg.org/files/100/100-0.txt")
+response.encoding = "utf-8-sig"
+content = response.text
+for char in ["\n", "\r", "\d", "\t"]:
+    content = content.replace(char, " ")
+
 split_content = re.split(r"\b", content)
-all_words = set(split_content)
-word2idx = {word: idx for idx, word in enumerate(all_words)}
-idx2word = {idx: word for idx, word in enumerate(all_words)}
+word2idx = dict()
+idx2word = dict()
+idx = 0
+for word in split_content:
+    if word not in word2idx:
+        word2idx[word] = idx
+        idx2word[idx] = word
+        idx += 1
 
-
-def tokenize(text):
+def tokenize(text: str):
     split = re.split(r"\b", text)
     return [word2idx[token] for token in split]
 
-
-def detokenize(indices):
-    return "".join([idx2word[idx.item()] for idx in indices])
-
+def detokenize(indices: torch.Tensor):
+    if indices.ndim == 1:
+        return "".join([idx2word[idx.item()] for idx in indices])
+    elif indices.ndim == 2:
+        return ["".join([idx2word[idx.item()] for idx in row]) for row in indices]
 
 if __name__ == "__main__":
     indices = []
@@ -27,4 +38,4 @@ if __name__ == "__main__":
     chunked_indices = [chunk for chunk in chunked_indices if len(chunk) == 970]
     chunked_indices = torch.stack(chunked_indices)
     chunked_indices = chunked_indices[:-1]
-    torch.save(chunked_indices, "shakespeare.pt")
+    torch.save(chunked_indices, "/Users/anshumanradhakrishnan/repos/deep_learning_curriculum/exercises/transformers/data/shakespeare.pt")
