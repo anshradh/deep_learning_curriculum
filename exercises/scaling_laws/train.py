333ba08736a6ee0c9d610071bcfa55d0ac975920
Ansh Radhakrishnan <anshuman.radhakrishnan@yale.edu>
2023-03-04 19:49:46 -0800

    Make script more automated

--- exercises/scaling_laws/train.py
+++ exercises/scaling_laws/train.py
@@ -11,14 +11,13 @@ import torchvision
 from torch.utils.data import DataLoader, Dataset, Subset
 from model import MNISTCNN, MNISTCNNConfig
 from argparse import ArgumentParser, Namespace
+import math
 
 
 def train(args):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
-
-    torch.manual_seed(args.seed + rank)
 
     device = torch.device(args.device, rank)
 
@@ -29,154 +28,199 @@ def train(args):
         dropout=args.dropout,
     )
 
-    model = MNISTCNN(config).to(device)
+    lr_correction_factor = math.sqrt(32) / math.sqrt(args.n_filters)
 
-    optimizer = optim.AdamW(
-        model.parameters(),
-        lr=args.learning_rate,
-        weight_decay=args.weight_decay,
-    )
+    start_seed = args.start_seed + rank
 
-    train_dataset = torchvision.datasets.MNIST(
-        root="data",
-        train=True,
-        download=True,
-        transform=torchvision.transforms.ToTensor(),
-    )
+    final_val_losses = []
 
-    train_dataset = Subset(
-        train_dataset,
-        torch.randperm(int(len(train_dataset) * args.dataset_fraction)).tolist(),
-    )
+    for s in range(start_seed, start_seed + args.num_seeds):
+        torch.manual_seed(s)
 
-    val_dataset = torchvision.datasets.MNIST(
-        root="data",
-        train=False,
-        download=True,
-        transform=torchvision.transforms.ToTensor(),
-    )
+        model = MNISTCNN(config).to(device)
 
-    local_train_dataset = Subset(
-        train_dataset,
-        range(
-            rank * len(train_dataset) // size,
-            (rank + 1) * len(train_dataset) // size,
-        ),
-    )
-
-    local_val_dataset = Subset(
-        val_dataset,
-        range(rank * len(val_dataset) // size, (rank + 1) * len(val_dataset) // size),
-    )
-
-    train_loader = DataLoader(
-        local_train_dataset,
-        batch_size=args.batch_size,
-        shuffle=True,
-        num_workers=1,
-        pin_memory=True,
-    )
-
-    val_loader = DataLoader(
-        local_val_dataset,
-        batch_size=args.batch_size,
-        shuffle=False,
-        num_workers=1,
-        pin_memory=True,
-    )
-
-    model.train()
-
-    hooks = []
-    for p in model.parameters():
-        comm.Bcast(p.data, root=0)
-        hooks.append(
-            p.register_hook(lambda grad: grad / size if grad is not None else grad)
+        optimizer = optim.AdamW(
+            model.parameters(),
+            lr=lr_correction_factor * args.learning_rate,
+            weight_decay=args.weight_decay,
         )
 
-    comm.Barrier()
+        train_dataset = torchvision.datasets.MNIST(
+            root="data",
+            train=True,
+            download=True,
+            transform=torchvision.transforms.ToTensor(),
+        )
 
-    if args.use_wandb:
-        import wandb
+        train_dataset = Subset(
+            train_dataset,
+            torch.randperm(int(len(train_dataset) * args.dataset_fraction)).tolist(),
+        )
 
-        wandb.init(project=args.wandb_project, config=args)
+        val_dataset = torchvision.datasets.MNIST(
+            root="data",
+            train=False,
+            download=True,
+            transform=torchvision.transforms.ToTensor(),
+        )
 
-    for epoch in tqdm(range(1, args.epochs + 1)):
-        for batch_idx, (data, target) in enumerate(train_loader):
-            data, target = data.to(device), target.to(device)
-            optimizer.zero_grad()
-            output = model(data)
-            loss = F.cross_entropy(output, target)
-            accuracy = (output.argmax(dim=1) == target).float().mean()
-            loss.backward()
+        local_train_dataset = Subset(
+            train_dataset,
+            range(
+                rank * len(train_dataset) // size,
+                (rank + 1) * len(train_dataset) // size,
+            ),
+        )
 
-            for p in model.parameters():
-                if p.grad is not None:
-                    comm.Allreduce(MPI.IN_PLACE, p.grad, op=MPI.SUM)
+        local_val_dataset = Subset(
+            val_dataset,
+            range(
+                rank * len(val_dataset) // size, (rank + 1) * len(val_dataset) // size
+            ),
+        )
 
-            comm.Barrier()
+        train_loader = DataLoader(
+            local_train_dataset,
+            batch_size=args.batch_size,
+            shuffle=True,
+            num_workers=1,
+            pin_memory=True,
+        )
 
-            optimizer.step()
+        val_loader = DataLoader(
+            local_val_dataset,
+            batch_size=args.batch_size,
+            shuffle=False,
+            num_workers=1,
+            pin_memory=True,
+        )
 
-            if args.use_wandb and rank == 0:
-                wandb.log(
-                    {
-                        "train_loss": loss.item(),
-                        "train_accuracy": accuracy.item(),
-                    }
-                )
+        model.train()
 
-            if args.print_interval is not None and batch_idx % args.print_interval == 0:
-                print(
-                    f"Train Epoch: {epoch} \tLoss: {loss.item():.6f}\tAccuracy: {accuracy.item():.6f}"
-                )
-
-            if args.save_interval is not None and batch_idx % args.save_interval == 0 and rank == 0:
-                torch.save(
-                    model.state_dict(),
-                    f"{args.save_path}mnist_model_{epoch}_{batch_idx}.pt",
-                )
-
-        with torch.no_grad():
-            model.eval()
-            val_loss = 0
-            val_accuracy = 0
-            for data, target in val_loader:
-                data, target = data.to(device), target.to(device)
-                output = model(data)
-                val_loss += F.cross_entropy(output, target, reduction="sum").item()
-                val_accuracy += (output.argmax(dim=1) == target).float().sum().item()
-            val_loss /= len(local_val_dataset)
-            val_accuracy /= len(local_val_dataset)
-            print(
-                f"Val set: Average loss: {val_loss:.4f}, Accuracy: {val_accuracy:.4f}"
+        hooks = []
+        for p in model.parameters():
+            comm.Bcast(p.data, root=0)
+            hooks.append(
+                p.register_hook(lambda grad: grad / size if grad is not None else grad)
             )
-            model.train()
-            if args.use_wandb and rank == 0:
-                wandb.log(
-                    {
-                        "val_loss": val_loss,
-                        "val_accuracy": val_accuracy,
-                    }
-                )
 
-    for hook in hooks:
-        hook.remove()
+        comm.Barrier()
+
+        if args.use_wandb:
+            import wandb
+
+            wandb.init(project=args.wandb_project, config=args)
+
+        for epoch in tqdm(range(1, args.epochs + 1)):
+            for batch_idx, (data, target) in enumerate(train_loader):
+                data, target = data.to(device), target.to(device)
+                optimizer.zero_grad()
+                output = model(data)
+                loss = F.cross_entropy(output, target)
+                accuracy = (output.argmax(dim=1) == target).float().mean()
+                loss.backward()
+
+                for p in model.parameters():
+                    if p.grad is not None:
+                        comm.Allreduce(MPI.IN_PLACE, p.grad, op=MPI.SUM)
+
+                comm.Barrier()
+
+                optimizer.step()
+
+                if args.use_wandb and rank == 0:
+                    wandb.log(
+                        {
+                            "train_loss": loss.item(),
+                            "train_accuracy": accuracy.item(),
+                        }
+                    )
+
+                if (
+                    args.print_interval is not None
+                    and batch_idx % args.print_interval == 0
+                ):
+                    print(
+                        f"Train Epoch: {epoch} \tLoss: {loss.item():.6f}\tAccuracy: {accuracy.item():.6f}"
+                    )
+
+                if (
+                    args.save_interval is not None
+                    and batch_idx % args.save_interval == 0
+                    and rank == 0
+                ):
+                    torch.save(
+                        model.state_dict(),
+                        f"{args.model_save_path}mnist_model_{epoch}_{batch_idx}.pt",
+                    )
+
+            with torch.no_grad():
+                model.eval()
+                val_loss = 0
+                val_accuracy = 0
+                for data, target in val_loader:
+                    data, target = data.to(device), target.to(device)
+                    output = model(data)
+                    val_loss += F.cross_entropy(output, target, reduction="sum").item()
+                    val_accuracy += (
+                        (output.argmax(dim=1) == target).float().sum().item()
+                    )
+                val_loss /= len(local_val_dataset)
+                val_accuracy /= len(local_val_dataset)
+
+                val_loss = comm.allreduce(val_loss, op=MPI.SUM) / size
+                val_accuracy = comm.allreduce(val_accuracy, op=MPI.SUM) / size
+
+                comm.Barrier()
+
+                if rank == 0:
+                    print(
+                        f"Val set: Average loss: {val_loss:.4f}, Accuracy: {val_accuracy:.4f}"
+                    )
+                model.train()
+                if args.use_wandb and rank == 0:
+                    wandb.log(
+                        {
+                            "val_loss": val_loss,
+                            "val_accuracy": val_accuracy,
+                        }
+                    )
+                if epoch == args.epochs and rank == 0:
+                    final_val_losses.append(val_loss.item())
+
+        for hook in hooks:
+            hook.remove()
+
+        if rank == 0:
+            torch.save(
+                model.state_dict(),
+                f"{args.model_save_path}mnist_model_final_n_filters_{config.n_filters}_dataset_frac_{args.dataset_fraction:3f}_seed_{args.seed}.pt",
+            )
+
+        if args.use_wandb and rank == 0:
+            wandb.finish()
+
+    avg_val_loss = sum(final_val_losses) / len(final_val_losses)
+    if rank == 0:
+        print(f"Average val loss: {avg_val_loss}")
 
     if rank == 0:
-      torch.save(
-          model.state_dict(), f"{args.save_path}mnist_model_final_n_filters_{config.n_filters}_dataset_frac_{args.dataset_fraction:3f}_seed_{args.seed}.pt"
-      )
-
-    if args.use_wandb and rank == 0:
-        wandb.finish()
+        torch.save(
+            dict(
+                loss=avg_val_loss,
+                n_filters=config.n_filters,
+                dataset_fraction=args.dataset_fraction,
+            ),
+            f"{args.results_save_path}mnist_model_final_n_filters_{config.n_filters}_dataset_frac_{args.dataset_fraction:3f}_results.pt",
+        )
 
 
 if __name__ == "__main__":
     arg_parser = ArgumentParser()
     arg_parser.add_argument("--device", type=str, default="cpu")
     arg_parser.add_argument("--device_count", type=int, default=1)
-    arg_parser.add_argument("--seed", type=int, default=0)
+    arg_parser.add_argument("--start_seed", type=int, default=0)
+    arg_parser.add_argument("--num_seeds", type=int, default=1)
     arg_parser.add_argument("--batch_size", type=int, default=64)
     arg_parser.add_argument("--learning_rate", type=float, default=1e-3)
     arg_parser.add_argument("--dataset_fraction", type=float, default=1.0)
@@ -190,7 +234,8 @@ if __name__ == "__main__":
     arg_parser.add_argument("--wandb_project", type=str, default="scaling_laws")
     arg_parser.add_argument("--print_interval", type=int, default=None)
     arg_parser.add_argument("--save_interval", type=int, default=None)
-    arg_parser.add_argument("--save_path", type=str, default="")
+    arg_parser.add_argument("--model_save_path", type=str, default="")
+    arg_parser.add_argument("--results_save_path", type=str, required=True)
 
     args = arg_parser.parse_args()
 
